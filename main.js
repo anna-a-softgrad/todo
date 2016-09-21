f35a4b5a3864786a20126d20dad1751a6cb5b5f4
anna-a-softgrad <anna-a@softgrad.com>
2016-09-21 14:12:40 +0300

    change js

--- main.js
+++ main.js
@@ -11,19 +11,43 @@ Container.prototype.addTask = function() {
 	// добавить его в массив 
 	this.tasksArray.push(taskItem);
 }
-
 Container.prototype.removeTask = function() {
 	// удалить из массива или списка элемент
 }
-
 Container.prototype.removeAllTasks = function() {
 	this.tasksArray = [];
 }
-
 Container.prototype.changeStateTask = function() {
 	Task.prototype.changeState();
 }
+Container.prototype.showAllTasks = function() {
+	var arr = this.tasksArray;
 
+	for (var i = 0; i < arr.length; i++) {
+  			// отобразить
+  		}
+	}
+}
+Container.prototype.showDoneTasks = function() {
+	// проверить все задачи на done
+	var arr = this.tasksArray;
+
+	for (var i = 0; i < arr.length; i++) {
+  		if (arr[i].isDone == true) {
+  			// отобразить
+  		}
+	}
+}
+Container.prototype.showUndoneTasks = function() {
+	// проверить все задачи на done
+	var arr = this.tasksArray;
+
+	for (var i = 0; i < arr.length; i++) {
+  		if (arr[i].isDone == false) {
+  			// отобразить
+  		}
+	}
+}
 
 /* Класс задач*/
 
